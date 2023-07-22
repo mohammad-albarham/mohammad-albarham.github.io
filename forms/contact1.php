c13e2d4bad45d0d65158c2e283c92e9ebd1d0aa7
PAIN-BARHAM <mohammedbrham98@gmail.com>
2023-07-23 01:55:28 +0400

    version # 1

---
+++ forms/contact1.php
@@ -0,0 +1,25 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $to = "albarham.mohammad@gmail.com"; // Replace with the recipient's email address
+  $subject = $_POST["subject"];
+  $name = $_POST["name"];
+  $email = $_POST["email"];
+  $message = $_POST["message"];
+
+  echo $subject;
+  echo $name;
+  echo $email;
+  echo $message
+  // Email headers
+  $headers = "From: $name <$email>" . "\r\n";
+  $headers .= "Reply-To: $email" . "\r\n";
+  $headers .= "Content-Type: text/plain; charset=utf-8" . "\r\n";
+
+  // Sending the email
+  if (mail($to, $subject, $message, $headers)) {
+    echo "Email sent successfully!";
+  } else {
+    echo "Failed to send the email. Please try again later.";
+  }
+}
+?>

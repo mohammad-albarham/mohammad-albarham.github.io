7bf0f771e408fba69892c07b3689264c3df3d33a
mohammad-albarham <mohammedbrham98@gmail.com>
2025-03-13 22:47:27 +0100

    Add travel map section :)

---
+++ assets/js/countries.js
@@ -0,0 +1,17 @@
+var countriesVisited = {
+  "Africa": ["TN"],
+  "Asia": ["JO", "PS", "SA", "AE", "TR"],
+  "Europe": ["PL", "CH", "AT", "DE", "MK", "SE", "TR"],
+  "North America": ["CA"],
+  "South America": [],
+};
+// From https://en.wikipedia.org/wiki/Continent#Number
+var continentColors = {
+  "Europe": "#c10000",
+  "North America": "#00cc00",
+  "South America": "#008000",
+  "Asia": "#f33e01",
+  "Africa": "#fed52e",
+  "Antarctica": "#0040ff",
+  "Oceania": "#c04080",
+}

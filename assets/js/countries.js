d5879f39863e4ec16bbe1eb2c557b0da1815aad3
MOHAMMAD ALBARHAM <mohammedbrham98@gmail.com>
2025-09-21 18:57:12 +0300

    Lithuania 	🇱🇹

--- assets/js/countries.js
+++ assets/js/countries.js
@@ -1,7 +1,7 @@
 var countriesVisited = {
   "Africa": ["TN"],
   "Asia": ["JO", "PS", "SA", "AE", "TR"],
-  "Europe": ["PL", "CH", "AT", "DE", "MK", "SE", "TR", "HU", "DK", "BA"],
+  "Europe": ["PL", "CH", "AT", "DE", "MK", "SE", "TR", "HU", "DK", "BA","LT"],
   "North America": ["CA"],
 };
 // From https://en.wikipedia.org/wiki/Continent#Number
